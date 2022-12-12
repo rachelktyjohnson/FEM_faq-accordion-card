ff8f8e07c6fe8dc3a511303178759f6db689acc9
Rachel Johnson <rachelktyjohnson@gmail.com>
2022-12-13 08:24:12 +1100

    desktop styling and js functionality

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const question_cards = document.querySelector(".question-cards");
+const question_box = document.querySelector('.desktop-box');
 
 question_cards.addEventListener('click', (event)=>{
     if (event.target.className === "question"){
         event.target.parentNode.classList.toggle("active");
+    }
+})
+
+question_cards.addEventListener('mouseover', (event)=>{
+    if (event.target.className === "question"){
+        question_box.style.left = "35px";
+    }
+})
+
+question_cards.addEventListener('mouseout', (event)=>{
+    if (event.target.className === "question"){
+        question_box.style.left = "75px";
     }
 })
